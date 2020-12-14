736a11a58b5b561ed5d2591bf9dc1177f5b3639c
wonseok <wonseok5893@naver.com>
2020-12-15 00:59:07 +0900

    피보나치 수2 dp 성공

---
+++ dp/Boj2748.java
@@ -0,0 +1,21 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2748 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[]dp = new long[91];
+
+        dp[1]= 1;
+        dp[2]= 1;
+        for (int i = 3; i <= 90; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
+    }
+}
