463f6e34062600430d10e682538cc7a36c9a721d
wonseok <wonseok5893@naver.com>
2020-12-22 22:35:13 +0900

    [FIX]

--- kakao/level2/Problem1.java
+++ kakao/level2/Problem1.java
@@ -4,28 +4,24 @@ import java.util.ArrayList;
 
 public class Problem1 {
     public static void main(String[] args) {
-        solution(2,new String[]{"Jeju", "Pangyo", "NewYork", "newyork"});
+        solution(5, new String[]{"SEOUL", "SEOUL", "SEOUL"});
     }
+
     public static int solution(int cacheSize, String[] cities) {
         int answer = 0;
         ArrayList<String> cacheArr = new ArrayList<>();
-        if(cacheSize!=0) {
+        if (cacheSize != 0) {
             for (int i = 0; i < cities.length; i++) {
                 String city = cities[i].toLowerCase();
-                if (i< cacheSize) {
+                if (i < cacheSize) {
+                    int removePos = check(cacheArr, city);
+                    if (removePos != -1) answer += 1;
+                    else
+                        answer += 5;
                     cacheArr.add(city);
-                    answer += 5;
                 } else {
-                    boolean result = true;
-                    int removePos = -1;
-                    for (int j = 0; j < cacheSize; j++) {
-                        if (cacheArr.get(j).equals(city)) {
-                            removePos = j;
-                            result = false;
-                            break;
-                        }
-                    }
-                    if (!result) {
+                    int removePos = check(cacheArr,  city);
+                    if (removePos != -1) {
                         cacheArr.remove(removePos);
                         answer += 1;
                     } else {
@@ -35,8 +31,23 @@ public class Problem1 {
                     cacheArr.add(city);
                 }
             }
-        }else answer = cities.length*5;
+        } else answer = cities.length * 5;
+        System.out.println(answer);
         return answer;
     }
+
+    private static int check(ArrayList<String> list, String city) {
+        boolean result = true;
+        int removePos = -1;
+        for (int j = 0; j < list.size(); j++) {
+            if (list.get(j).equals(city)) {
+                removePos = j;
+                result = false;
+                break;
+            }
+        }
+        return removePos;
+    }
+
 
 }
