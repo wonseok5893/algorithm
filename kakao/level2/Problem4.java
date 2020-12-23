4711b05a54b6d14a6edbf5b52580c5f6e3db8055
wonseok <wonseok5893@naver.com>
2020-12-23 19:42:05 +0900

    kakaolevel1 problem1 stack

---
+++ kakao/level2/Problem4.java
@@ -0,0 +1,11 @@
+package kakao.level2;
+
+public class Problem4 {
+    public static void main(String[] args) {
+
+    }
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        return answer;
+    }
+}
