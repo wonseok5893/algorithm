fc249756d42dc3e28514a02e9c3f831f1d61c515
wonseok <wonseok5893@naver.com>
2021-03-19 19:16:07 +0900

    Boj2133 - 3xn 타일링

---
+++ dp/Boj2133.java
@@ -0,0 +1,23 @@
+package dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Boj2133 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        //3 x n
+        // n == 1
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 2; i <=n; i+=2) {
+            dp[i] += dp[i-2];
+            for (int j = i-2; j >=0 ; j-=2) {
+                dp[i] += dp[j]*2;
+            }
+        }
+        System.out.println(dp[n]);
+
+    }
+}
